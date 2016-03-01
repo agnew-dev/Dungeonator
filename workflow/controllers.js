5ced16a6826adf765a9616688f37ba067809eb6a
yeah-cheddar <kogcheddarxm@gmail.com>
2016-02-29 19:51:44 -0500

    Configured locations object with chrome storage syncs

--- workflow/controllers.js
+++ workflow/controllers.js
@@ -6,7 +6,47 @@ angular.module('dungeonator.controllers', ['ngResource', 'ngRoute'])
 
 	}])
 	.controller('LocationsController', ['$scope', '$resource', '$route', function($scope, $resource, $route){
+		$scope.locations = new Object;
 
+		$scope.addLocation = function(locationName){
+			chrome.storage.sync.get({locations:[]}, function(result){
+				var locations = result.locations;
+				var newLoc = {
+					id: locations.length + 1,
+					name: locationName,
+					people: [ {
+						personName: String,
+						personrace: String,
+						personHP: Number,
+						personDescription: String,
+						personItems: []
+					}
+				],
+				quests:[{
+					questName: String,
+					questGiver: String,
+					questTasks: [String]
+				}],
+				map: String,
+				overview: String
+			};
+			locations.push(newLoc);
+			chrome.storage.sync.set({locations: locations}, function(){
+				chrome.storage.sync.get('locations', function(result){
+					$scope.locations = result.locations;
+				});
+			});
+			});
+		};
+
+		// INIT locations
+		$scope.initLocations = function(){
+			chrome.storage.sync.get({locations: []}, function(results){
+				$scope.locations = results.locations;
+			});
+		};
+
+		$scope.initLocations();
 	}])
 	.controller('ChaptersController', ['$scope', '$resource', '$route', function($scope, $resource, $route){
 
